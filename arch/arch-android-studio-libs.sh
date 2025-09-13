3823dc3081bf301647a24ad45241206a834aae42
NamesMT <dangquoctrung123@gmail.com>
2025-09-13 11:43:55 +0000

    feat: add `arch-android-studio-libs` script

---
+++ arch/arch-android-studio-libs.sh
@@ -0,0 +1,3 @@
+# These seems to be the packages that is necessary for android studio to launch and run emulator
+sudo pacman -S --needed --noconfirm \
+  fontconfig freetype2 jdk-openjdk libbsd libglvnd libice libpulse libsm libx11 libxcb libxcomposite libxcursor libxdamage libxext libxfixes libxi libxkbcommon libxkbcommon-x11 libxkbfile libxrandr libxrender libxtst mesa noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-dejavu vulkan-icd-loader vulkan-mesa-layers xcb-util xcb-util-cursor xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm
